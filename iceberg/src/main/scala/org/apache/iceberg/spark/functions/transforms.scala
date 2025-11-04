c4f620b764e2e6fb1ac5115983cfba1c5c41ccbd
Chong Gao <res_life@163.com>
2025-11-04 20:40:59 +0800

    Fix building error
    Signed-off-by: Chong Gao <res_life@163.com>

--- iceberg/src/main/scala/org/apache/iceberg/spark/functions/transforms.scala
+++ iceberg/src/main/scala/org/apache/iceberg/spark/functions/transforms.scala
@@ -61,13 +61,13 @@ object GpuTransform {
     if (transform.startsWith("bucket")) {
       val bucket = transform.substring("bucket[".length, transform.length - 1).toInt
       GpuBucket(bucket)
-    } if (transform.startsWith("year")) {
+    } else if (transform.startsWith("year")) {
       GpuYears
-    } if (transform.startsWith("month")) {
+    } else if (transform.startsWith("month")) {
       GpuMonths
-    } if (transform.startsWith("day")) {
+    } else if (transform.startsWith("day")) {
       GpuDays
-    } if (transform.startsWith("hour")) {
+    } else if (transform.startsWith("hour")) {
       GpuHours
     } else {
       throw new IllegalArgumentException(s"Unsupported transform: $transform")
